3487a836597f92554846cabf15f3e828acf5ba6c
middletaq@gmail.com <middletaq@gmail.com>
2024-09-03 22:24:31 -0400

    update

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   const navItems = [
-    'Home', 'Experience', 'Projects', 'Certifications', 'Skills', 'Education', 'Contact'
+    'Home', 'Experience', 'Projects', 'Certifications', 
   ];
 
   return (
