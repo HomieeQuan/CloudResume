d1559e3826450ab1e26a0afa523125ea74996f83
middletaq@gmail.com <middletaq@gmail.com>
2024-09-24 22:51:30 -0400

    update

--- Backend/database.py
+++ Backend/database.py
@@ -2,3 +2,14 @@ import os
 from dotenv import load_dotenv
 from azure.cosmos import CosmosClient, PartitionKey
 load_dotenv()
+
+URL = os.getenv('COSMOS_URI')
+KEY = os.getenv('COSMOS_KEY')
+DATABASE_NAME = os.getenv('COSMOS_DATABASE')
+CONTAINER_NAME = os.getenv('COSMOS_CONTAINER')
+
+
+# Initialize the Cosmos client
+client = CosmosClient(URL, credential=KEY)
+database = client.get_database_client(DATABASE_NAME)
+container = database.get_container_client(CONTAINER_NAME)
